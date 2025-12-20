e29f6b3d698c332e81bef34b77f33129bcb7ed6b
Rufus Pollock <rufus@lifeitself.org>
2025-12-20 15:15:58 +0800

    feat: add csv parsing utility

---
+++ tiptap-v1/lib/csv.ts
@@ -0,0 +1,9 @@
+import Papa from 'papaparse'
+
+export function parseCsvText(text: string): string[][] {
+  const result = Papa.parse<string[]>(text.trim(), { skipEmptyLines: true })
+  if (result.errors.length > 0) {
+    throw new Error(result.errors[0].message)
+  }
+  return result.data
+}
